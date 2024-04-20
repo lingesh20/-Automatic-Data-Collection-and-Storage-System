1e7aac45d443af51b364584bf0e7926b7191c81a
Lingeshwaran <63338272+lingesh20@users.noreply.github.com>
2024-04-20 12:53:26 +0530

    Add files via upload

--- lambda-to-slackapi/lambda_function.py
+++ lambda-to-slackapi/lambda_function.py
@@ -6,7 +6,9 @@ def lambda_handler(event, context):
     # Check if the event indicates that the alarm has been triggered
     if event.get('alarm_triggered'):
         # Send a message to the Slack channel
-        send_slack_message("Alarm triggered! Take action.")
+        message = event.get('message', 'No message provided')
+        send_slack_message(message)
+        # send_slack_message("Data successfully inserted in ")
         
     return {
         'statusCode': 200,
@@ -16,7 +18,8 @@ def lambda_handler(event, context):
 def send_slack_message(message):
     slack_token = os.environ["slack_token"]
     slack_channel = "C06STLJV7S5"  # Replace with your Slack channel name or ID
-    slack_url = f"https://hooks.slack.com/services/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"
+    slack_url = f"https://hooks.slack.com/services/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"
+    # slack_url = f"https://hooks.slack.com/services/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"
     
     headers = {
         "Authorization": f"Bearer {slack_token}",
